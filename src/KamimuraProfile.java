221019dfa8d9e3cc289d660c1ef8ca64b6164d80
Kamimura <tarako2000yen@yahoo.co.jp>
2015-07-13 19:48:27 +0900

    test3

---
+++ src/KamimuraProfile.java
@@ -0,0 +1,11 @@
+public class SuzukiProfile extends Profile {
+
+	public KamimuraProfile(String name) {
+		super(name);
+	}
+
+	@Override
+	public String profile() {
+		return "éÑÇÃéÔñ°ÇÕÇ†Ç†Ç†Ç≈Ç∑ÅB";
+	}
+}
